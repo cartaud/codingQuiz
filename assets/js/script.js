f57bdf1263e4c358c4ed9a74761025e75d0d47c0
Chadd <chadd_artaud@cox.net>
2022-04-09 15:13:30 -0700

    created quiz logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,33 +22,85 @@ let questions = [
         ]
     },
     {
-        question: "Commonly used data types DO include:",
+        question: "The condition in an if / else statement is enclosed within ____.",
         answers: [
-            {option: "1. strings", value: false},
-            {option: "2. booleans", value: false},
-            {option: "3. alerts", value: true},
-            {option: "4. numbers", value: false}
+            {option: "1. quotes", value: false},
+            {option: "2. curly brackets", value: false},
+            {option: "3. parentheses", value: true},
+            {option: "4. square brackets", value: false}
         ]
-    }
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answers: [
+            {option: "1. numbers and strings", value: false},
+            {option: "2. other arrays", value: false},
+            {option: "3. booleans", value: false},
+            {option: "4. all of the above", value: true}
+        ]
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: [
+            {option: "1. commas", value: false},
+            {option: "2. curly brackets", value: false},
+            {option: "3. quotes", value: true},
+            {option: "4. parentheses", value: false}
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            {option: "1. JavaScript", value: false},
+            {option: "2. terminal / bash", value: false},
+            {option: "3. for loops", value: false},
+            {option: "4. console log", value: true}
+        ]
+    },
 ]
+let questionX = 0;
 
 function startingQuiz() {
     remove()
     countDown()
     container.append(question);
     container.append(options);
-    let i=0
-    while (i < questions.length) {
-        question.textContent = questions[i].question;
-        for (let j=0;j<4;j++){
-            let btn = document.createElement('button');
-            options.append(btn);
-            btn.classList.add('answer');
-            btn.textContent = 'kill ';
+    showQuestion();
+}
+
+function showQuestion() {
+    // question global variable for looping through 
+    options.innerHTML = '';
+    question.innerHTML = questions[questionX].question;
+    for (let i=0;i<4;i++) {
+        var btn = document.createElement('button');
+        options.append(btn);
+        btn.classList.add('answer');
+        btn.setAttribute('data-value', questions[questionX].answers[i].value)
+        btn.textContent = questions[questionX].answers[i].option;
+        btn.addEventListener('click', next)
+    }
+    function next() {
+        questionX++
+        if (questionX >= questions.length ) {
+            endQuiz()
         }
-        
+        else if (this.attributes[1].value == 'true') {
+            console.log('u right');
+            showQuestion();
+        }
+        else {
+            console.log('u wrong');
+            showQuestion();
+        }
     }
 }
+
+function endQuiz() {
+    remove();
+    console.log('you finished!')
+}
+
 
 function remove() {
     let child = container.lastElementChild;
